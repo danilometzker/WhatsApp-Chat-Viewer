49807cc46577a619d1906a168a2aeeff8fcc18b7
null <null>
2025-11-27 08:42:20 -0400

    feat: test

--- src/Components/ChatMenu/index.tsx
+++ src/Components/ChatMenu/index.tsx
@@ -1,5 +1,15 @@
+import * as stylex from '@stylexjs/stylex';
+
 const ChatMenu = () => {
-  return <></>;
+  return <div {...stylex.props(styles.base)}></div>;
 };
 
 export default ChatMenu;
+
+const styles = stylex.create({
+  base: {
+    backgroundColor: 'red',
+    width: 50,
+    height: 50,
+  },
+});
